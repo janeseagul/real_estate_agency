af5e8c608bfbb49ee4fc0803a0f411aa6b01a189
Marta Chaika <janeseagul@gmail.com>
2023-04-04 16:20:15 +0300

    edit field name

--- property/models.py
+++ property/models.py
@@ -83,8 +83,8 @@ class Complaint(models.Model):
 
 class Owner(models.Model):
     flat_owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
-    owner_pure_phone = PhoneNumberField(
+    phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    pure_phone = PhoneNumberField(
         'Нормализованный номер владельца',
         null=True, blank=True,
         db_index=True)
