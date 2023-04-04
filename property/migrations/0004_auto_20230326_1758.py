3469ce848f87357dd18ef7eac8febd868496d27c
Marta Chaika <janeseagul@gmail.com>
2023-04-04 19:16:24 +0300

    Update 0004_auto_20230326_1758.py

--- property/migrations/0004_auto_20230326_1758.py
+++ property/migrations/0004_auto_20230326_1758.py
@@ -5,9 +5,8 @@ from django.db import migrations
 
 def define_new_buildings(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all().iterator():
-        Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
-        Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 class Migration(migrations.Migration):
 
